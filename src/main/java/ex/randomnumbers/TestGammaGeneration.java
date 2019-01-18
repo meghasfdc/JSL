3f9fd59692b653478bfc0b113aa88559dd37b8ef
Manuel Rossetti <rossetti@uark.edu>
2019-01-18 14:49:38 -0600

    Just some timing of the gamma generation

--- src/main/java/ex/randomnumbers/TestGammaGeneration.java
+++ src/main/java/ex/randomnumbers/TestGammaGeneration.java
@@ -22,17 +22,44 @@ import jsl.utilities.random.rvariable.JSLRandom;
 import jsl.utilities.reporting.JSL;
 
 import java.io.PrintWriter;
+import java.time.Duration;
+import java.time.Instant;
 
 public class TestGammaGeneration {
 
+    private static PrintWriter outAR = JSL.makePrintWriter("gammaOutAR", "txt");
+    private static PrintWriter outInv = JSL.makePrintWriter("gammaOutInv", "txt");
+
     public static void main(String[] args) {
 
-        PrintWriter out = JSL.makePrintWriter("gammaOut", "txt");
         RNStreamIfc stream = RNStreamFactory.getDefaultStream();
-        for (int i = 1; i <= 10000; i++) {
-            //double x = JSLRandom.rGamma(3.0, 5.0, stream, JSLRandom.AlgoType.AcceptanceRejection);
-            double x = JSLRandom.rGamma(3.0, 5.0, stream);
-            out.println(x);
+
+        long arTime = runGammaAR(1000000, stream);
+
+        long invTime = runGammaInv(1000000, stream);
+
+        System.out.printf("AR time = %d %n", arTime);
+        System.out.printf("Inv time = %d %n", invTime);
+        System.out.printf("Inv time - AR time = %d %n", (invTime-arTime));
+    }
+
+    public static long runGammaAR(int n, RNStreamIfc stream) {
+        Instant start = Instant.now();
+        for (int i = 1; i <= n; i++) {
+            double x = JSLRandom.rGamma(3.0, 5.0, stream, JSLRandom.AlgoType.AcceptanceRejection);
         }
+        Instant finish = Instant.now();
+        long timeElapsed = Duration.between(start, finish).toMillis();
+        return timeElapsed;
+    }
+
+    public static long runGammaInv(int n, RNStreamIfc stream) {
+        Instant start = Instant.now();
+        for (int i = 1; i <= n; i++) {
+            double x = JSLRandom.rGamma(3.0, 5.0, stream);
+        }
+        Instant finish = Instant.now();
+        long timeElapsed = Duration.between(start, finish).toMillis();
+        return timeElapsed;
     }
 }
